88c60047eb415106e343796acbe6a8b0d34792cc
Pedro Severo <ctw03221@L-VRW540WG07.local>
2023-11-07 14:32:13 +0000

    checkpoint

---
+++ src/pages/login/LoginPage.styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+export const LoginPageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,13 +4,14 @@ import { PATH } from "./paths";
 
 //  TODO: Remove this imports and their usage after to start project
 import { Link } from "react-router-dom";
+import { LoginPage } from "../pages/login/LoginPage";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={PATH.HOME} element={<Link to={PATH.LOGIN}>Login</Link>} />
-        <Route path={PATH.LOGIN} element={<Link to={PATH.HOME}>Home</Link>} />
+        <Route path={PATH.LOGIN} element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
