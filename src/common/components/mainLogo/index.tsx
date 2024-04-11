a61c9f6e25d87472407a013f9c9105d3bdac0b1c
Pedro Severo <phsevero94@gmail.com>
2024-04-11 18:33:37 +0100

    adding todo

--- src/common/components/mainLogo/index.tsx
+++ src/common/components/mainLogo/index.tsx
@@ -11,6 +11,7 @@ export const MainLogo = () => {
         <Suspense
             fallback={
                 <designSystem.circularProgressIndeterminate
+                    // TODO: color primary is not getting the correct color settled on themeProvider
                     color="primary"
                     dataTestId="main-logo-lazy-loading"
                 />
