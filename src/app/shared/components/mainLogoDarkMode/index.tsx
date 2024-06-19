2138034103646a02d67a4e30f8773c939cc8a953
Pedro Severo <53839013+pedro-severo@users.noreply.github.com>
2024-06-19 16:50:38 -0300

    feat/home page (#33)

---
+++ src/app/shared/components/mainLogoDarkMode/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Suspense, lazy } from 'react';
+import designSystem from '../../designSystem';
+
+// istanbul ignore next
+const MainLogoDarkModeView = lazy(() =>
+    import('./view').then(({ MainLogoDarkModeView }) => ({
+        default: MainLogoDarkModeView,
+    }))
+);
+
+export const MainLogoDarkMode = () => {
+    return (
+        <Suspense
+            fallback={
+                <designSystem.circularProgressIndeterminate
+                    color="primary"
+                    dataTestId="main-logo-lazy-loading"
+                />
+            }
+        >
+            <MainLogoDarkModeView />
+        </Suspense>
+    );
+};
